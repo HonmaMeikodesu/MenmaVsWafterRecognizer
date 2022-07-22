915c66b2d71d66a8cd0a6b0f49700bfc972c6a6d
HonmaMeiko <17mwang2@stu.edu.cn>
2022-07-23 02:21:20 +0800

    init cnn model

--- src/main.py
+++ src/main.py
@@ -1,2 +1,41 @@
-import tensorflow as tf
-print(tf.config.list_physical_devices('GPU'))
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+import keras
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(16, (3,3), activation='relu', 
+                           input_shape=(250, 250, 3)),
+    keras.layers.MaxPooling2D(2, 2),
+    keras.layers.Conv2D(32, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer=RMSprop(learning_rate=0.001), metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(
+        "..\\training_set\\",
+        target_size=(250, 250),
+        batch_size=100,
+        class_mode='binary')
+
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+validation_generator = validation_datagen.flow_from_directory(
+        '..\\test_set\\',
+        target_size=(250, 250), 
+        batch_size=25,
+        class_mode='binary')
+
+history = model.fit(
+      train_generator,
+      validation_data = validation_generator,  
+      epochs=10,
+      steps_per_epoch=4,
+      validation_steps=3,
+      verbose=1)
--- src/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-(tf.config.list_physical_devices('GPU'))
